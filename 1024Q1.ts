9fd3c243f3995aa2551223955c106f80fe5b13e9
TT <taian921215@gmail.com>
2024-10-24 11:40:07 +0800

    no message

--- 1024Q1.ts
+++ 1024Q1.ts
@@ -1,4 +1,5 @@
 const age: number = 25; //10241.ts:4:1 - error TS2588: Cannot assign to 'age' because it is a constant.
+                        //在JS中 const 被改為 var
 let Name: string = "Alice";
 
 age += 1;
